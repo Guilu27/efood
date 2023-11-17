d69780abe383ee9b86459f186b42392c58f2dea5
Guilherme <guilherme@exemplo.com>
2023-11-17 15:27:46 -0300

    feat(api): Renderização da RestaurantPage via Redux
    - Agora a página do restaurante (RestaurantPage) é renderizada com sucesso usando o Redux e os dados da API.

--- src/pages/RestaurantPage/index.tsx
+++ src/pages/RestaurantPage/index.tsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import CardsList from '../../components/CardsList'
 
-import { Restaurant } from '../Home'
+import { useGetRestaurantQuery } from '../../services/api'
 
 const RestaurantPage = () => {
   const { id } = useParams()
 
-  const [restaurant, setRestaurant] = useState<Restaurant[]>([])
+  const { data } = useGetRestaurantQuery(id ?? '1')
+  const restaurant = data ? [data] : []
 
-  useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
-      .then((res) => res.json())
-      .then((res) => setRestaurant([res]))
-  }, [id])
-
-  return (
-    <div>
-      <Header type="restaurant" />
-      <Banner restaurant={restaurant} />
-      <div className="container">
-        <CardsList type="foods" restaurants={restaurant} />
+  if (restaurant) {
+    return (
+      <div>
+        <Header type="restaurant" />
+        <Banner restaurant={restaurant} />
+        <div className="container">
+          <CardsList type="foods" restaurants={restaurant} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  return <h4>Carregando...</h4>
 }
 
 export default RestaurantPage
